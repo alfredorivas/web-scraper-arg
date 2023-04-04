e14ee8bcb67bc4467a2c39153215bd73bdc8835c
Alfredo Rivas <alfredo.rivas@mercadolibre.com>
2023-04-03 23:06:06 -0300

    build of general profile to generate urls

--- src/main.py
+++ src/main.py
@@ -7,14 +7,14 @@ logging.getLogger().setLevel(logging.DEBUG)
 
 
 def main():
+    with open('./profiles/general.yaml') as f:
+        url_data = yaml.safe_load(f)
     names = ['argenprop', 'meli', 'zonaprop']
     for name in names:
         logging.info(' :::::::::::::::::::::::::')
         logging.info(' :: Processing ' + name)
-        with open('./profiles/' + name + '.yaml') as f:
-            url_data = yaml.safe_load(f)
-            url_data = url_data['url-data']
-        url = url_data['base'] + url_data['type'] + url_data['ops'] + url_data['size'] + url_data['pri-loc'] + url_data['sec-loc'] + url_data['price-range'] + url_data['end']
+        url_data_tmp = url_data['url-data-' + name]
+        url = url_data_tmp['base'] + url_data_tmp['type'] + url_data_tmp['ops'] + url_data_tmp['size'] + url_data_tmp['pri-loc'] + url_data_tmp['sec-loc'] + url_data_tmp['price-range'] + url_data_tmp['end']
         logging.info('  :: URL: ' + url)
         site = NewSite(name, url)
         site.output()
